59ecb568c0f3d14e2de83411352a349d2a8fbe6c
sevda <sevdasazdar@gmail.com>
2020-05-03 16:24:50 +0430

    fix js cods

--- main.js
+++ main.js
@@ -1,23 +1,11 @@
-var x = document.getElementById("buton");
-var hamberger = document.getElementById("hamberger");
-var content = document.getElementById("hambergerMenu-content");
-function opend(){
-    
-    if(hamberger.style.display = "block"){
-        content.style.display = "block";
-    }
-    
-    else{
-        content.style.display += "none";
 
+  function opend() {
+    var content = document.getElementById("hambergerMenu-content");
+    if (content.style.display === "none") {
+      content.style.display = "block";
+    } else {
+      content.style.display = "none";
     }
-    
-    x.addEventListener('click',function(){
-        if(content.style.display = "block"){
-             content.style.display = "none";
-        }else{
-            content.style.display += "block";
-        }
-            });
-        
-}
+  }
+  
+
